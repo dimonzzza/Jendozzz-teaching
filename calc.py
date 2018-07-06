1609e0297e4733e9c403e94b0c995f70c560115a
dimonzzza <puzanov.rostov@yandex.ru>
2018-07-06 14:32:09 +0300

    fix

--- calc.py
+++ calc.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 while True:
-	x, z, y = raw_input().split() 
-	x = float(x)
-	y = float(y)
+	x = raw_input()
+	if x == 'quit':
+		quit()
+	else:
+		z = raw_input()
+		y = raw_input()
+		x = float(x)
+		y = float(y)
 	if z in ('+','-','*','/','//','%','**'):	
 		if z == '+':
 			print(x + y)
@@ -12,13 +18,18 @@ while True:
 		elif z == '*':
 			print(x * y)
 		elif z == '/':
-			print(x / y)
+			if y != 0:
+				print(x / y)
+			else:
+				print('Деление на ноль!')
 		elif z == '//':
 			print(x // y)
 		elif z == '%':
 			print(x % y)
 		elif z == '**':
 			print(x ** y)
-
+	else:
+		print('Неверный знак операции')
+	
 
 
