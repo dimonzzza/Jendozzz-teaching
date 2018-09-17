9f879fd4cfbcfcc41cc021d81c72741fbc5afb24
dimonzzza <puzanov.rostov@yandex.ru>
2018-09-18 01:10:00 +0300

    fix

--- news.py
+++ news.py
@@ -12,9 +12,18 @@ except EnvironmentError:
 	sys.exit()
 
 parse = bs4.BeautifulSoup(site.text, "html.parser")
-latest_news = parse.select(".title .storylink")
+last_news = parse.select(".title .storylink")
 
 print('Последние новости news.ycombinator.com:')
-for i in range(len(latest_news)):
-	print(i+1, latest_news[i].getText())
+
+if len(sys.argv) == 2:
+	last = int(sys.argv[1])
+	last_news = last_news[:last]
+if len(sys.argv) == 3:
+	first = int(sys.argv[1])
+	last = int(sys.argv[2])
+	last_news = last_news[first:last]
+
+for i in range(len(last_news)):
+	print(i+1, last_news[i].getText())
 
