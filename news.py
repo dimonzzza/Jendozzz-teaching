b46b42a6478e35a7ea56b4d6b2794794d93979ae
dimonzzza <puzanov.rostov@yandex.ru>
2018-09-17 14:20:00 +0300

    fix

--- news.py
+++ news.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests, bs4
-site=requests.get('https://news.ycombinator.com/')
-parse=bs4.BeautifulSoup(site.text, "html.parser")
-latest_news=parse.select(".title .storylink")
+import requests
+import bs4
+import sys
+
+try:
+	site = requests.get('https://news.ycombinator.com')
+except EnvironmentError:
+	print('not connection')
+	sys.exit()
+
+parse = bs4.BeautifulSoup(site.text, "html.parser")
+latest_news = parse.select(".title .storylink")
+
 print('Последние новости news.ycombinator.com:')
 for i in range(len(latest_news)):
-	print(i, latest_news[i].getText())
+	print(i+1, latest_news[i].getText())
 
