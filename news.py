f02353f69f4dfe66938b06a62b40339815fe6eff
dimonzzza <puzanov.rostov@yandex.ru>
2018-09-22 01:08:51 +0300

    fix

--- news.py
+++ news.py
@@ -13,6 +13,7 @@ except EnvironmentError:
 
 parse = bs4.BeautifulSoup(site.text, "html.parser")
 last_news = parse.select(".title .storylink")
+points = parse.select(".subtext .score")
 
 print('Последние новости news.ycombinator.com:')
 
@@ -24,6 +25,14 @@ if len(sys.argv) == 3:
 	last = int(sys.argv[2])
 	last_news = last_news[first:last]
 
-for i in range(len(last_news)):
-	print(i+1, last_news[i].getText())
+a = last_news + points
+result_list = []
+
+for i in range(len(a)):
+	dictionary = {'name': last_news[i],'point': points[i]}
+	result_list.append(dictionary)
+
+print(result_list)
+
+	
 
